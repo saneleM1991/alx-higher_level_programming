c75a11ea9d63e647d40b213647bde76a26a47d5c
saneleM1991 <91sanelem@gmail.com>
2023-08-08 16:23:08 +0200

    5-print_comb2 script fix 1

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i < 10:
-        print("0{:d},".format(i), end=" ")
+    if i != 99:
+        print('{num:02d},'.format(num=i), end=" ")
     else:
-        print("{:d},".format(i), end=" ")
+        print("{}\n".format(i))
