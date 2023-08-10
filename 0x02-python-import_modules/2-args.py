55508d0b0c994e341b255a4c4e38914d7ba459ba
saneleM1991 <91sanelem@gmail.com>
2023-08-10 20:20:06 +0200

    2-args.py fix script 1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-import sys
-
-number_of_arguments = len(sys.argv)
-args_index = 1
-if number_of_arguments == 1:
-    print("0 arguments.")
-else:
-    if number_of_arguments == 2:
-        print("1 argument:")
-        print(f"1: {sys.argv[1]}")
+if __name__ == "__main__":
+    import sys
+    number_of_arguments = len(sys.argv)
+    args_index = 1
+    if number_of_arguments == 1:
+        print("0 arguments.")
     else:
-        actual_args = len(range(1, number_of_arguments))
-        print(f"{actual_args} arguments:")
-        for i in range(1, number_of_arguments):
-            print(f"{args_index}: {sys.argv[i]}")
-            args_index += 1
+        if number_of_arguments == 2:
+            print("1 argument:")
+            print(f"1: {sys.argv[1]}")
+        else:
+            actual_args = len(range(1, number_of_arguments))
+            print(f"{actual_args} arguments:")
+            for i in range(1, number_of_arguments):
+                print(f"{args_index}: {sys.argv[i]}")
+                args_index += 1
