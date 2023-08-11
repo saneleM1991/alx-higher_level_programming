e22c171d0ce001be185056d294f427585ad31d11
saneleM1991 <91sanelem@gmail.com>
2023-08-11 15:31:43 +0200

    4-new_in_list.py script added

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    """replaces an element in a list at a specific
+        position without modifying the original list.
+
+    Args:
+        my_list: list of elements
+        idx: index of an element in alist
+        element: element in alist
+
+    Return:
+        Copy of original list is idx < 0 or idx is out of range.
+
+        Modifide copy of original list.
+    """
+    my_list_copy = my_list[:]
+    if idx < 0:
+        return my_list_copy
+    if idx > (len(my_list) - 1):
+        return my_list_copy
+    my_list_copy[idx] = element
+    return my_list_copy
