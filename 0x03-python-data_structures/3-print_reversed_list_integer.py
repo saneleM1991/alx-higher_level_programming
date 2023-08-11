ebee152c94f56a2a8ce2396ff92286a8e289ca81
saneleM1991 <91sanelem@gmail.com>
2023-08-11 15:13:34 +0200

    3-print_reversed_list_integer.py script added

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    """Print elements in reversed order.
+
+    Args:
+        my_list: list of elements
+    """
+    for i in my_list[::-1]:
+        print("{}".format(i))
