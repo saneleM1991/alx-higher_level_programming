621e781594eed0060e1cd5a7fcdc7faed298d1ec
saneleM1991 <91sanelem@gmail.com>
2023-08-16 09:58:59 +0200

    3-common_elements.py script added

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    """Find common elements in twoo set and return them.
+
+    Args:
+        set_1: Ste 1
+        set_2: Set 2
+
+    Returns:
+            New set with common elements
+    """
+    return set_1 & set_2
