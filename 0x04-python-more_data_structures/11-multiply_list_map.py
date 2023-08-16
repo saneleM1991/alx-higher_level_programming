0e6d561640632a07591de35ce6e9554722e80e76
saneleM1991 <91sanelem@gmail.com>
2023-08-16 23:31:36 +0200

    11-multiply_list_map.py script fix 1

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    """Multiply each number in a list.
-
-    Args:
-        my_list: list[]
-        number: Number to multiply each number with
-
-    Returns:
-            New list []
-    """
     return (list(map((lambda i: i * number), my_list)))
