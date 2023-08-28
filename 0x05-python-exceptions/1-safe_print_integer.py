287e8e1610ff8856e4cdda3a49c9009e16dc2a76
saneleM1991 <91sanelem@gmail.com>
2023-08-28 15:31:53 +0200

    1-safe_print_integer.py script fixer 1.4

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -13,19 +13,3 @@ def safe_print_integer(value):
         return True
     except ValueError:
         return False
-
-
-value = 89
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = -89
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = "School"
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
