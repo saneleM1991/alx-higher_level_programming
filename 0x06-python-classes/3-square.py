b1927e7b87fab6e8baa72015f0086b1153c3b7d8
saneleM1991 <91sanelem@gmail.com>
2023-08-29 16:48:54 +0200

    3-square.py script added

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -28,3 +28,20 @@ class Square:
             Area (`int`) : size * size.
         """
         return self.__size * self.__size
+
+    @property
+    def size(self):
+        """:obj:int: Returns `size` of a square.
+
+        Setter raise an `TyprError` if `value` is not int
+        or raise `ValueError` if `value` < 0
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
