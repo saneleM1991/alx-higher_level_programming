08330d46d3475e47c34cdd648c87ef90d378fa7f
saneleM1991 <91sanelem@gmail.com>
2023-08-29 13:58:02 +0200

    1-square.py script added

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square module that has a class to create square object."""
+
+
+class Square:
+    """A empty square class."""
+
+    def __init__(self, size):
+        """Init method for the square class.
+
+        Args:
+            size (int): size of a sqaure.
+        """
+        self._size = size
