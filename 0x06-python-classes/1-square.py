f6b40a73830369d1e39974a9f8b2bf73ac886235
Sanele <91sanelem@gmail.com>
2023-08-29 15:25:19 +0200

    1-square.py doc string addited

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,12 +3,12 @@
 
 
 class Square:
-    """A empty square class."""
+    """An empty square class."""
 
     def __init__(self, size):
         """Init method for the square class.
 
         Args:
-            size (int): size of a sqaure.
+            size (int): size of a square.
         """
         self.__size = size
