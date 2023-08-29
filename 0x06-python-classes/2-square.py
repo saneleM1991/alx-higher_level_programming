f93fc883abbb4c800bcdf1b1819cb000e16ab2ec
Sanele <91sanelem@gmail.com>
2023-08-29 15:50:55 +0200

    2-square.py script added

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Square module that has a class to create square object."""
+
+
+class Square:
+    """An empty square class."""
+
+    def __init__(self, size=0):
+        """Init method for the square class.
+
+        Args:
+            size (:obj:`int`, optional): size of a square.
+
+        Raises:
+            TypeError: if `size` is not an `int`.
+            ValueError: If `size` is < 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
