8a8c1bad3ae07aba4baf2c377df22caf9e295d96
saneleM1991 <91sanelem@gmail.com>
2023-09-05 00:37:03 +0200

    2-rectangle.py script edited

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -78,17 +78,3 @@ class Rectangle:
         for i in range(self.__height):
             print("#" * self.__width)
         return ""
-
-
-my_rectangle = Rectangle(2, 4)
-print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
-
-print(str(my_rectangle))
-print(repr(my_rectangle))
-
-print("--")
-
-my_rectangle.width = 10
-my_rectangle.height = 3
-print(my_rectangle)
-print(repr(my_rectangle))
