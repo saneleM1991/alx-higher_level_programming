e83ad072901ca91bb8f8ddf79fcf81ea9b70feca
saneleM1991 <91sanelem@gmail.com>
2023-09-05 00:17:06 +0200

    2-rectangle.py script edited

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -67,6 +67,6 @@ class Rectangle:
         Returns:
                 `int` Perimeter of rectangle
         """
-        if self.width == 0 or self.__height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * (self.width + self.__height)
+        return 2 * (self.__width + self.__height)
