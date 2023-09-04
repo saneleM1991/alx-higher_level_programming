cd89dbff369f43a8330877bd07df67e6a9a6d32b
saneleM1991 <91sanelem@gmail.com>
2023-09-05 00:38:36 +0200

    3-rectangle.py script added

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -70,3 +70,11 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Print object using # sign."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        for i in range(self.__height):
+            print("#" * self.__width)
+        return ""
