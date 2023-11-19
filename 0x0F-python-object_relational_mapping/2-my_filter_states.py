a215c7c8b5d214731e692ea8b65fd958a230d5eb
saneleM1991 <91sanelem@gmail.com>
2023-11-19 16:37:55 +0200

    2-my_filter_states.py script fix2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,8 @@ if __name__ == "__main__":
                            passwd=sys.argv[2], db=sys.argv[3],
                            charset="utf8")
     cur = conn.cursor()
-    cur.execute(f"SELECT * FROM states WHERE name = '{sys.argv[4]}'")
+    cur.execute(
+        f"SELECT * FROM states WHERE name = '{sys.argv[4]}' ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
