98844c4818259e7dac8fbe44773c22a9781aad42
saneleM1991 <91sanelem@gmail.com>
2023-11-19 16:34:05 +0200

    2-my_filter_states.py script added

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""2my filter states from hbtn_0e_0_usa database."""
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3],
+                           charset="utf8")
+    cur = conn.cursor()
+    cur.execute(f"SELECT * FROM states WHERE name = '{sys.argv[4]}'")
+    query_rows = cur.fetchall()
+
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
