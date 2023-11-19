c883e158e15cc9b771870b6b67f0111be868ba1a
saneleM1991 <91sanelem@gmail.com>
2023-11-19 16:09:42 +0200

    1-filter_states.py script fixer2

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """1-filter states from hbtn_0e_0_usa database."""
+
 if __name__ == "__main__":
-    import sys
     import MySQLdb
+    import sys
 
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3],
